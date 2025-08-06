bc68a6966dd7e08898cc3c08f5b0dfdae258e31c
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-08-06 20:53:55 +0000

    Release 0.2.8

--- src/main/java/com/devintest/api/core/ClientOptions.java
+++ src/main/java/com/devintest/api/core/ClientOptions.java
@@ -32,10 +32,10 @@ public final class ClientOptions {
         this.headers.putAll(headers);
         this.headers.putAll(new HashMap<String, String>() {
             {
-                put("User-Agent", "io.github.devalog:devin-java/0.2.7");
+                put("User-Agent", "io.github.devalog:devin-java/0.2.8");
                 put("X-Fern-Language", "JAVA");
                 put("X-Fern-SDK-Name", "com.devintest.fern:api-sdk");
-                put("X-Fern-SDK-Version", "0.2.7");
+                put("X-Fern-SDK-Version", "0.2.8");
             }
         });
         this.headerSuppliers = headerSuppliers;
